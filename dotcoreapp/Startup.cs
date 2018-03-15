94e677b03fed5243bf93f1df86d774de937a0f26
D Jegadeesh.51682384 <jegadeeshd@hcl.com>
2018-03-15 14:13:54 +0530

    intial check-in

---
+++ dotcoreapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(dotcoreapp.Startup))]
+namespace dotcoreapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
